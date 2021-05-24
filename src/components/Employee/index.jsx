1cdc622e96f5287bbc900403966c94da006f4ccd
roeicohen10 <63965618+roeicohen10@users.noreply.github.com>
2021-05-24 18:56:37 +0300

    Add files via upload

--- src/components/Employee/index.jsx
+++ src/components/Employee/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from './styles.module.scss'
 const Employee = ({ name, status }) => {
+  const isVacation = (status=="On Vacation")
   return (
     <div>
       <div className={styles.container}>
         <span>{name}</span>
-        <span>{status}</span>
+        {isVacation ? (
+          <span className={styles.vacationStatus}>{status}</span>
+        ): (
+          <span className={styles.regularStatus}>{status}</span>
+        )}
       </div>
     </div>
   );
